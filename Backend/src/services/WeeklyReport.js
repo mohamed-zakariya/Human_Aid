7daeb810ef3b186fb10be7a456cf9668f3063dc8
Youssef Ashraf <114014804+Youssef-Ashraf02@users.noreply.github.com>
2025-06-10 20:18:49 +0300

    commit

--- Backend/src/services/WeeklyReport.js
+++ Backend/src/services/WeeklyReport.js
@@ -3,16 +3,15 @@ import dotenv from 'dotenv';
 import { weeklyReportJob } from '../config/weeklyReportJob.js';
 import { MONGO_URL } from "../config/envConfig.js";
 
-// Load environment variables
+
 dotenv.config();
 
-// Connect to your MongoDB
 const runJob = async () => {
   try {
     await mongoose.connect(MONGO_URL);
     console.log('MongoDB connected.');
 
-    await weeklyReportJob(); // Run your report generation and email sending
+    await weeklyReportJob(); 
 
     await mongoose.disconnect();
     console.log('MongoDB disconnected. Job complete.');
